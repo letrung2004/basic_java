ab084b9fc9d0be3e7cd5ad18797704cd655a701b
letrung2004 <127586077+letrung2004@users.noreply.github.com>
2024-05-06 00:45:42 +0700

    Add files via upload

---
+++ CauHinh.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai3;
+
+
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author USER
+ */
+public class CauHinh {
+    public int soMui=0;
+    public static final DateTimeFormatter FORMATTER= DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static final Scanner sc=new Scanner(System.in);
+}
